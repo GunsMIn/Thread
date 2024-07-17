bd69c355204308a02d2263c2564c0ae34173b8d2
KimGunWoo9595 <medneter@naver.com>
2024-07-18 00:20:49 +0900

    쓰레드 join()

---
+++ src/main/java/com/example/javathread/util/ThreadUtils.java
@@ -0,0 +1,14 @@
+package com.example.javathread.util;
+
+import static com.example.javathread.util.MyLogger.log;
+
+public abstract class ThreadUtils {
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log("인터럽트 발생, " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
