4ba11503b8d7ef25169bf50926fbbc01639f52c7
medneter <medneter@naver.com>
2024-07-31 14:08:48 +0900

    volatile

---
+++ src/main/java/com/example/javathread/thread/volatil1/VolatilFlagMain.java
@@ -0,0 +1,32 @@
+package com.example.javathread.thread.volatil1;
+
+import static com.example.javathread.util.MyLogger.log;
+import static com.example.javathread.util.ThreadUtils.sleep;
+
+public class VolatilFlagMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+        //boolean runFlag = true;
+        volatile boolean runFlag = true;
+        //volatile boolean runFlag = true;
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+            log("task 종료");
+        }
+    }
+}
